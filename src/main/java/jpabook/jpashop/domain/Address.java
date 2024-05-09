2ef2a9bc41480160ae2628fb21d500dd7b2db4c8
Soocone <wltn3185@naver.com>
2024-05-09 23:27:43 +0900

    5/9 commit

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,9 +12,11 @@ public class Address {
     private String street;
     private String zipcode;
 
+    // 함부로 new로 생성하는 것을 방지하기 위함
     protected Address() {
     }
 
+    // setter를 쓰지 않고 생성할 때만 값이 세팅되도록 생성자 추가
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
